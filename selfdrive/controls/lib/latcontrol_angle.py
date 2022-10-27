260d96bacfd838f837b29f20c03c7a9973038345
Cameron Clough <cameronjclough@gmail.com>
2022-10-27 00:17:16 -0700

    try this

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -33,15 +33,15 @@ class LatControlAngle(LatControl):
     #angle_log.steeringAngleDeg = float(CS.steeringAngleDeg)
     #angle_log.steeringAngleDesiredDeg = desired_curvature
 
-    actual_curvature = VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
+    actual_curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
     error = desired_curvature - actual_curvature
     freeze_integrator = steer_limited or CS.steeringPressed or CS.vEgo < 5
-    out = self.pid.update(error,
-                          feedforward=desired_curvature,
-                          speed=CS.vEgo,
-                          freeze_integrator=freeze_integrator)
+    output = -self.pid.update(error,
+                              feedforward=desired_curvature,
+                              speed=CS.vEgo,
+                              freeze_integrator=freeze_integrator)
 
     if not angle_log.active:
-      out = 0
+      output = 0
 
-    return 0, out, angle_log
+    return 0, output, angle_log
